f77aa551c557d57b4456838128e7ba2bbd27499d
zocanw1 <lunox.id33@gmail.com>
2026-07-02 21:17:56 +0700

    cleanup: remove dead files, old models, duplicates, empty dirs, move logs

--- start-stella.ps1
+++
@@ -1,8 +0,0 @@
-$projectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-Set-Location $projectRoot
-
-Write-Host "Menjalankan Stella dari: $projectRoot"
-Write-Host "Tekan Ctrl+C untuk menghentikan bot."
-
-node index.js
